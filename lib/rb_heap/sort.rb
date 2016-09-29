29b9303e0b96c9f3e8031b9273add056bd37e059
Florian Hartmann <jscoderr@gmail.com>
2016-09-29 21:10:07 +0200

    Implement in-place heapsort

--- lib/rb_heap/sort.rb
+++ lib/rb_heap/sort.rb
@@ -1,16 +1,28 @@
 class Heap
-  def self.sort(array)
-    heap = Heap.new
-    result = Array.new(array.size)
+  def self.sort(array, order = :<, &compare_fn)
+    compare_fn = self.invertComparison(order, &compare_fn)
+    heap = Heap.new(order, array, &compare_fn)
 
     array.each do |element|
       heap.add(element)
     end
 
-    (0...array.size).each do |i|
-      result[i] = heap.pop
+    (0...array.size).reverse_each do |i|
+      array[i] = heap.pop
     end
 
-    result
+    heap.to_a
+  end
+
+  def self.invertComparison(order, &compare_fn)
+    if block_given?
+      lambda{|a, b| not compare_fn.call(a, b)}
+    elsif order == :< or order.nil?
+      lambda{|a, b| a > b}
+    elsif order == :>
+      lambda{|a, b| a < b}
+    else
+      raise ArgumentError.new("The comparison symbol needs to be either :> or :<")
+    end
   end
 end
