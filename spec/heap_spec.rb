4247523d299733b240bff5eb710223a35e013325
Florian Hartmann <jscoderr@gmail.com>
2016-09-29 19:54:44 +0200

    Complete the unit tests

--- spec/heap_spec.rb
+++ spec/heap_spec.rb
@@ -9,14 +9,6 @@ describe Heap do
     expect(Heap::VERSION).not_to be nil
   end
 
-  describe :add do
-
-  end
-
-  describe :<< do
-
-  end
-
   describe :peak do
     it "returns the top element" do
       @a << 3 << 2 << 5
@@ -34,7 +26,37 @@ describe Heap do
   end
 
   describe :new do
+    it "creates a min heap by default" do
+      a = Heap.new
+      a << 3 << 2 << 5
+      expect(a.pop).to eq(2)
+      expect(a.pop).to eq(3)
+      expect(a.pop).to eq(5)
+    end
 
+    it "can create a min heap using :<" do
+      a = Heap.new(:<)
+      a << 3 << 2 << 5
+      expect(a.pop).to eq(2)
+      expect(a.pop).to eq(3)
+      expect(a.pop).to eq(5)
+    end
+
+    it "can create a min heap using :<" do
+      a = Heap.new(:>)
+      a << 3 << 2 << 5
+      expect(a.pop).to eq(5)
+      expect(a.pop).to eq(3)
+      expect(a.pop).to eq(2)
+    end
+
+    it "can create a custom heap using a block" do
+      a = Heap.new{|a, b| a.abs < b.abs}
+      a << 3 << -2 << -5
+      expect(a.pop).to eq(-2)
+      expect(a.pop).to eq(3)
+      expect(a.pop).to eq(-5)
+    end
   end
 
   describe :size do
@@ -61,19 +83,88 @@ describe Heap do
   end
 
   describe :empty? do
+    it "returns true when the heap is empty" do
+      expect(@a.empty?).to be true
+    end
 
+    it "returns false when the heap has elements" do
+      @a << 1
+      expect(@a.empty?).to be false
+    end
+
+    it "returns false when we elements are added after being removed" do
+      @a << 1
+      @a.pop
+      @a << 2
+      expect(@a.empty?).to be false
+    end
   end
 
   describe :pop do
+    it "returns the top element" do
+      @a << 2 << 1 << 3
+      expect(@a.pop).to eq(1)
+    end
 
+    it "removes the top element" do
+      @a << 2 << 1 << 3 << 5 << 4 << 8 << 6 << 7 << 9
+
+      (1..9).each do |i|
+        expect(@a.pop).to eq(i)
+      end
+    end
+
+    it "returns nil when the heap is empty" do
+      expect(@a.pop).to be nil
+    end
   end
 
   describe :replace do
+    it "replaces the top element" do
+      @a << 2 << 1 << 3 << 5 << 4 << 8 << 6 << 7 << 9
+      @a.replace(0)
+      expect(@a.peak).to eq(0)
+    end
 
+    it "does not change the heap size" do
+      @a << 2 << 1 << 3 << 5 << 4 << 8 << 6 << 7 << 9
+      oldSize = @a.size
+      @a.replace(0)
+      expect(@a.size).to eq(oldSize)
+    end
+
+    it "rebalances the heap" do
+      @a << 2 << 1 << 3 << 5 << 4 << 8 << 6 << 7 << 9
+      @a.replace(10)
+      expect(@a.peak).to eq(2)
+    end
   end
 
   describe :offer do
+    it "does not replace the top if the given element is not in relation with it" do
+      @a << 5 << 3 << 4
+      @a.offer(2) # 2 < 3 is true, so it should not be replaced
+      expect(@a.pop).to eq(3)
+      expect(@a.pop).to eq(4)
+      expect(@a.pop).to eq(5)
+    end
 
+    it "does replace the top if the given element is in relation with it" do
+      @a << 5 << 3 << 4
+      @a.offer(6) # 6 < 3 is false, so it should be replaced
+      expect(@a.pop).to eq(4)
+      expect(@a.pop).to eq(5)
+      expect(@a.pop).to eq(6)
+    end
+
+    it "respects the given order relation" do
+      a = Heap.new{|a, b| a.abs < b.abs}
+      a << -3 << -5 << 4
+      a.offer(-10) # -10.abs < -3.abs is false, so it should be replaced
+      expect(a.pop).to eq(4)
+      expect(a.pop).to eq(-5)
+      expect(a.pop).to eq(-10)
+    end
   end
 
   describe :clear do
@@ -85,6 +176,14 @@ describe Heap do
   end
 
   describe :to_a do
+    it "returns the heap as an array" do
+      @a << 2 << 1 << 3 << 4 << 6 << 5
+      # We have to sort because we can't make assumptions about the order
+      expect(@a.to_a.sort).to eq([1,2,3,4,5,6])
+    end
 
+    it "contains no meta elements" do
+      expect(@a.to_a).to eq([])
+    end
   end
 end
